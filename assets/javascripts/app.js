5813af2016cd3244e2298ebca112f5f38f0bd1cf
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-12-20 15:31:25 -0500

    added mathjax, made some formatting changes

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -7,14 +7,15 @@
  * ========================================================================
  */
 /*jshint strict:true unused:true*/
-/*global $ console*/
+/*global $*/
 
 ;(function () {
   'use strict';
 
-  // TODO: rewrite
+  // TODO: refactor
 
-  var show_result = function(button, form) {
+  // Shows results of last submission at the given button and form.
+  var showResult = function(button, form) {
     return function(result) {
       if (result) {
         button.addClass('success');
@@ -27,38 +28,39 @@
   };
 
   // Adds click listeners to the submit buttons.
-  var submit = function() {
+  var observeSubmitButton = function() {
 
     $('a.button.submit').click(function(e) {
       var button = $(e.target);
       var form = button.parents('form');
       var id = form.find('input.q-id').val();
-      $.post('/verify/quiz/' + id, form.serialize(), show_result(button, form));
+      $.post('/verify/quiz/' + id, form.serialize(), showResult(button, form));
       return false;
     });
 
   };
 
   // Adds click listeners to the run buttons.
-  var run = function() {
+  // var run = function() {
+  //   $('a.button.run').click(function(e){
+  //     var form = $(e.target).parents('form');
+  //     var raw = form.find('.ex-raw').val();
+  //     var id = form.find('.ex-id').val();
+  //     $.post('/verify/code/' + id, raw,
+  //       function(data) { console.log(data); }
+  //     );
+  //     return false;
+  //   });
+  // };
 
-    $('a.button.run').click(function(e){
-      var form = $(e.target).parents('form');
-      var raw = form.find('.ex-raw').val();
-      var id = form.find('.ex-id').val();
-      $.post('/verify/code/' + id, raw,
-        function(data) { console.log(data); }
-      );
-      return false;
-    });
+  var genie = {
+
+    launch: function() {
+      observeSubmitButton();
+    }
 
   };
 
-  var listen = function() {
-    run();
-    submit();
-  };
-
-  $(listen);
+  $(genie.launch());
 
 })();
